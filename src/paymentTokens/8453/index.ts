2638a65064d481182b86901444983d0989e431fe
Garrett Lee <garr.c.lee@gmail.com>
2024-09-27 16:23:38 -0500

    tokiemon gold

--- src/paymentTokens/8453/index.ts
+++ src/paymentTokens/8453/index.ts
@@ -12,6 +12,15 @@ const defaultPaymentTokens: PaymentToken[] = [
       "https://raw.githubusercontent.com/alma-labs/tokiemon-lists/main/assets/tokens/USDC.png",
   },
   {
+    name: "GOLD",
+    symbol: "Tokiemon Gold",
+    address: "0xF3105616aE4B17d4Cd053Da449F38d23c647D15F",
+    chainId: 8453,
+    decimals: 18,
+    logoURI:
+      "https://raw.githubusercontent.com/alma-labs/tokiemon-lists/main/assets/tokens/GOLD.png",
+  },
+  {
     name: "DAI",
     symbol: "DAI",
     address: "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
