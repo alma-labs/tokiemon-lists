4c267fba5c391d1f6263cc165e30190783979b7c
Garrett Lee <garr.c.lee@gmail.com>
2025-05-22 17:12:35 -0500

    paint

--- src/prompts/paintbrushes.ts
+++ src/prompts/paintbrushes.ts
@@ -5,7 +5,7 @@ export const paintbrushes: Paintbrush[] = [
     itemId: "745",
     name: "Ninja",
     selectionType: "random",
-    keywords: ["Black ninja pokemon", "Ninja pokemon"],
+    keywords: ["Ninja pokemon"],
   },
   {
     itemId: "746",
@@ -17,19 +17,19 @@ export const paintbrushes: Paintbrush[] = [
     itemId: "747",
     name: "Starry",
     selectionType: "always",
-    keywords: ["Celestial Starry Pokemon"],
+    keywords: ["Starry Pokemon"],
   },
   {
     itemId: "748",
     name: "Fossil",
     selectionType: "random",
-    keywords: ["Skeleton Pokemon", "Bone Pokemon"],
+    keywords: ["Skeleton Pokemon"],
   },
 
   {
     itemId: "749",
     name: "Electric",
     selectionType: "always",
-    keywords: ["Electric Lightning Pokemon"],
+    keywords: ["Electric Pokemon"],
   },
 ];
