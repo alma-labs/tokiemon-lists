bed962a5dec96d26bd6da6ed854dee3490f22bce
Cooper <55swimmer55@gmail.com>
2025-05-27 14:27:39 -0400

    add BASEDMC breed and prompts

---
+++ src/prompts/BASEDMC.ts
@@ -0,0 +1,18 @@
+export const degen = [
+    "minecraft zombie",
+    "minecraft enderman giant monster",
+    "minecraft creeper giant monster",
+    "minecraft enderman monster",
+  ];
+  
+  export const dragon = [
+    "minecraft creeper with wings and claws",
+    "minecraft enderman with wings and claws",
+    "minecraft creeper with wings",
+  ];
+  
+  export const kawaii = [
+    "minecraft creeper kawaii",
+    "minecraft creeper baby",
+    "minecraft enderman baby",
+  ];
