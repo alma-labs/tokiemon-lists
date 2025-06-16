92a5bad9ed69477cdaae5f092270daffefd2d44d
Cooper <55swimmer55@gmail.com>
2025-06-16 12:22:19 -0400

    add captureOnly flag to CommunityToken interface for hunting update

--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,7 @@ export interface CommunityToken {
     | "LIMITED"
   >;
   extraPaymentTokens?: PaymentToken[];
+  captureOnly?: boolean;
   inactive?: boolean;
   prompts?: {
     kawaii?: string[];
